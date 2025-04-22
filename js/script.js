a38ff6c6f337e139422321cdb889226060b4002d
Maximiliano Vidal <vidal.maximiliano@gmail.com>
2025-04-22 12:34:57 -0300

    Index y contacto, web y mobile

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   const toggleBtn = document.getElementById("toggle-header");
-  const header = document.querySelector(".header"); // cambia ID por clase
+  const header = document.querySelector(".header");
 
   toggleBtn.addEventListener("click", () => {
     header.classList.toggle("expandido");
