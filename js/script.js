cadd5cf3a1c2f6cd928bd518f3ec8ce519c82bc6
Maximiliano Vidal <vidal.maximiliano@gmail.com>
2025-04-27 18:21:48 -0300

    Ofertas ok, se crea archivo .json

--- js/script.js
+++ js/script.js
@@ -5,4 +5,32 @@ document.addEventListener("DOMContentLoaded", function () {
   toggleBtn.addEventListener("click", () => {
     header.classList.toggle("expandido");
   });
+
+
+fetch('../js/datos-cartas.json')
+.then(response => response.json())
+.then(data => {
+  const tbody = document.querySelector('#tabla-cartas tbody');
+  data.forEach(carta => {
+    const fila = `
+      <tr>
+        <td>${carta.vendedor}</td>
+        <td>${carta.idioma}</td>
+        <td>${carta.estado}</td>
+        <td>${carta.departamento}</td>
+        <td>${carta.barrio}</td>
+        <td>${carta.precio}</td>
+        <td>${carta.venta}</td>
+        <td>${carta.canje}</td>
+        <td>${carta.ofertas}</td>
+        <td>${carta.comprar}</td>
+      </tr>
+    `;
+    tbody.insertAdjacentHTML('beforeend', fila);
+  });
+})
+.catch(error => console.error('Error cargando datos:', error));
 });
+
+
+  
